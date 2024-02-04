3c0d9fba58ddd6005b81470f7cbd67abf1f1a49f
work2fly <jamie_glendinning@work2fly.co.uk>
2024-02-04 15:18:39 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,38 +1,25 @@
 // create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-// create a comments array
-var comments = [
-  {name: "John", comment: "I love this place!"},
-  {name: "Sarah", comment: "This place is the best!"},
-  {name: "Steve", comment: "I love this place!"}
-];
-
-// create a route to get all comments
-app.get('/comments', function(req, res) {
-  res.json(comments);
-});
-
-// create a route to add a comment
-app.post('/comments', function(req, res) {
-  var newComment = req.body;
-  comments.push(newComment);
-  res.json(newComment);
-});
-
-// start server on port 3000
-app.listen(3000, function() {
-  console.log('Server running on http://localhost:3000');
-});
-
-//In this example, we have created a simple web server using Express. We have created a comments array to store the comments, and we have created routes to get all comments and add a new comment. We have also used the `body - parser` middleware to parse the request body as JSON.
-
-//To run this example, save the code in a file called `comments.js` and run the following command in your terminal:
-
-
-   // `node comments.js
-
-// ` This will start the server on port 3000. You can then use a tool like Postman to make requests to the server and test the routes. For example, you can use a `GET` request to `http://localhost:3000/comments` to get all comments, and you can use a `POST` request to `http://localhost:3000/comments` with a JSON body to add a new comment.
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const comments = require('./comments');
+const commentsData = comments.commentsData;
+const commentsCount = comments.commentsCount;
+const commentsDataByPage = comments.commentsDataByPage;
+const commentsCountByPage = comments.commentsCountByPage;
+const commentsDataByPageAndLimit = comments.commentsDataByPageAndLimit;
+const commentsCountByPageAndLimit = comments.commentsCountByPageAndLimit;
+const commentsDataByPageAndLimitAndOrder = comments.commentsDataByPageAndLimitAndOrder;
+const commentsCountByPageAndLimitAndOrder = comments.commentsCountByPageAndLimitAndOrder;
+const commentsDataByPageAndLimitAndOrderAndSearch = comments.commentsDataByPageAndLimitAndOrderAndSearch;
+const commentsCountByPageAndLimitAndOrderAndSearch = comments.commentsCountByPageAndLimitAndOrderAndSearch;
+const commentsDataByPageAndLimitAndOrderAndSearchAndDate = comments.commentsDataByPageAndLimitAndOrderAndSearchAndDate;
+const commentsCountByPageAndLimitAndOrderAndSearchAndDate = comments.commentsCountByPageAndLimitAndOrderAndSearchAndDate;
+const commentsDataByPageAndLimitAndOrderAndSearchAndDateAndDate = comments.commentsDataByPageAndLimitAndOrderAndSearchAndDateAndDate;
+const commentsCountByPageAndLimitAndOrderAndSearchAndDateAndDate = comments.commentsCountByPageAndLimitAndOrderAndSearchAndDateAndDate;
+const commentsDataByPageAndLimitAndOrderAndSearchAndDateAndDateAndDate = comments.commentsDataByPageAndLimitAndOrderAndSearchAndDateAndDateAndDate;
+const commentsCountByPageAndLimitAndOrderAndSearchAndDateAndDateAndDate = comments.commentsCountByPageAndLimitAndOrderAndSearchAndDateAndDateAndDate;
+const commentsDataByPageAndLimitAndOrderAndSearchAndDateAndDateAndDateAndDate = comments.commentsDataByPageAndLimitAndOrderAndSearchAndDateAndDateAndDateAndDate;
+const commentsCountByPageAndLimitAndOrderAndSearchAndDateAndDateAndDateAndDate = comments.commentsCountByPageAndLimitAndOrderAndSearchAndDateAndDateAndDateAndDate;
+const commentsDataByPageAndLimitAndOrderAndSearchAndDateAndDateAndDateAndDateAndDate = comments.commentsDataByPageAndLimitAndOrderAndSearch
