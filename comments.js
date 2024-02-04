a43e858f98eaf3598a967aa7e130330459dc1b6b
work2fly <jamie_glendinning@work2fly.co.uk>
2024-02-04 15:15:24 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -27,14 +27,12 @@ app.post('/comments', function(req, res) {
 app.listen(3000, function() {
   console.log('Server running on http://localhost:3000');
 });
-```
 
-In this example, we have created a simple web server using Express. We have created a comments array to store the comments, and we have created routes to get all comments and add a new comment. We have also used the `body-parser` middleware to parse the request body as JSON.
+//In this example, we have created a simple web server using Express. We have created a comments array to store the comments, and we have created routes to get all comments and add a new comment. We have also used the `body - parser` middleware to parse the request body as JSON.
 
-To run this example, save the code in a file called `comments.js` and run the following command in your terminal:
+//To run this example, save the code in a file called `comments.js` and run the following command in your terminal:
 
-```
-node comments.js
-```
 
-This will start the server on port 3000. You can then use a tool like Postman to make requests to the server and test the routes. For example, you can use a `GET` request to `http://localhost:3000/comments` to get all comments, and you can use a `POST` request to `http://localhost:3000/comments` with a JSON body to add a new comment.
+   // `node comments.js
+
+// ` This will start the server on port 3000. You can then use a tool like Postman to make requests to the server and test the routes. For example, you can use a `GET` request to `http://localhost:3000/comments` to get all comments, and you can use a `POST` request to `http://localhost:3000/comments` with a JSON body to add a new comment.
